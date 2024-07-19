6bf34fa0d58ca1bb867d01432403010c3cd2d26d
rolandmorvai <mroland90@gmail.com>
2024-07-19 22:14:05 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'vercel-test';
+  private http = inject(HttpClient);
+
+  pokemons = new Observable<any>();
+
+  ngOnInit() {
+    this.pokemons = this.http.get<Observable<any>>('https://pokeapi.co/api/v2/pokemon/ditto');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { localeInterceptor } from './locale.interceptor';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([localeInterceptor]))],
 };
