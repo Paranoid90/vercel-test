b7e53ae3561fc789017e9d75f239dceaa0e0183b
rolandmorvai <mroland90@gmail.com>
2024-07-19 11:14:09 +0200

    interceptor added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LocaleInterceptor } from './locale.interceptor';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LocaleInterceptor,
+      multi: true
+    }
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
